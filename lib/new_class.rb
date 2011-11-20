8b86ce0996803913500e4525641bbc220802c67c
Paul Engel <paul.engel@holder.nl>
2011-11-20 05:49:03 +0100

    Removed 'var' and overriding method_missing and respond_to? instead Updated test/class_test.rb

--- lib/new_class.rb
+++ lib/new_class.rb
@@ -7,17 +7,15 @@ module NewClass
   end
 
   module ClassMethods
-    def when_defined(&block)
-      @when_defined = block
-    end
-
     def new_class(variables = {}, name = nil)
       Class.new(self).tap do |klass|
         klass.extend NewClassMethods
+        klass.extend MethodMissing
         klass.send :include, NewClassInstanceMethods
+        klass.send :include, MethodMissing
         klass.instance_variable_set :@name, name || self.name
-        klass.instance_variable_set :@variables, variables
-        klass.instance_eval &@when_defined if @when_defined
+        klass.instance_variable_set :@variables, variables.inject({}){|h, (k, v)| h.merge k.to_sym => v}
+        klass.defined if klass.respond_to?(:defined)
       end
     end
 
@@ -27,14 +25,22 @@ module NewClass
       end
       alias :to_s :name
 
-      def var(key)
-        (@variables || {})[key]
-      end
+      attr_reader :variables
     end
 
     module NewClassInstanceMethods
-      def var(key)
-        self.class.var(key)
+      def variables
+        self.class.variables
+      end
+    end
+
+    module MethodMissing
+      def method_missing(method, *args)
+        variables.include?(key = method.to_sym) ? define_method(key){ variables[key] }.call : super
+      end
+
+      def respond_to?(symbol, include_private = false)
+        variables.include?(symbol.to_sym) || super
       end
     end
   end
--- test/class_test.rb
+++ test/class_test.rb
@@ -11,32 +11,84 @@ class ClassTest < Test::Unit::TestCase
       end
     end
 
-    should "respond to when_defined and new_class" do
+    should "respond to new_class" do
       assert !A.respond_to?(:new_class)
-      assert !A.respond_to?(:when_defined)
       assert B.respond_to?(:new_class)
-      assert B.respond_to?(:when_defined)
+    end
+
+    should "be able to return a new class" do
+      assert_equal Class, B.new_class.class
+    end
+
+    context "the new class" do
+      setup do
+        @B = B.new_class
+        @C = B.new_class({}, "C")
+      end
+
+      should "have @name and @variables defined" do
+        assert !A.instance_variable_defined?(:@name)
+        assert !A.instance_variable_defined?(:@variables)
+
+        assert_equal(B.name, @B.instance_variable_get(:@name))
+        assert_equal({}, @B.instance_variable_get(:@variables))
+
+        assert_equal("C", @C.instance_variable_get(:@name))
+        assert_equal({}, @C.instance_variable_get(:@variables))
+      end
+
+      should "override method_missing and respond_to?" do
+        klass = B.new_class "foo" => "bar"
+
+        assert klass.respond_to?(:foo)
+        assert_equal "bar", klass.foo
+
+        assert klass.new.respond_to?(:foo)
+        assert_equal "bar", klass.new.foo
+
+        assert !klass.respond_to?(:bar)
+        assert_raises NoMethodError do
+          klass.bar
+        end
+
+        assert !klass.new.respond_to?(:bar)
+        assert_raises NoMethodError do
+          klass.new.bar
+        end
+      end
+
+      should "be able to create instances" do
+        assert @B.new.is_a?(B)
+        assert_equal B.name, @B.new.class.name
+        assert_equal B.name, @B.new.class.to_s
+
+        assert @C.new.is_a?(B)
+        assert_equal "C", @C.new.class.name
+        assert_equal "C", @C.new.class.to_s
+      end
     end
 
     context "and having a superclass" do
       setup do
         class Greeter
           @@translations = {}
+
           def self.translate(language, translation)
             @@translations[language.to_sym] = translation
           end
           def self.translation(language)
             @@translations[language.to_sym]
           end
-          translate :english, "Hi, I am a {class}"
           def greet(language = :english)
             self.class.translation(language).gsub("{class}", self.class.name.match(/[^:]+$/).to_s)
           end
+
+          translate :english, "Hi, I am a {class}"
         end
         class C < Greeter
           include NewClass
-          when_defined do
-            (var(:translations) || {}).each do |language, translation|
+          def self.defined
+            translations.each do |language, translation|
               translate language, translation
             end
           end
@@ -47,57 +99,15 @@ class ClassTest < Test::Unit::TestCase
         assert_equal "Hi, I am a C", C.new.greet
       end
 
-      should "be able to return a new class" do
-        assert_equal Class, C.new_class.class
-      end
+      should "have defined called" do
+        @C = C.new_class({:translations => {:dutch => "Hallo, ik ben een {class}"}})
+        @D = C.new_class({:translations => {:dutch => "Hallo, ik ben een {class}"}}, "D")
 
-      context "the new class" do
-        should "have @name and @variables defined" do
-          assert C.new_class.instance_variable_defined?(:@name)
-          assert_equal(C.name, C.new_class.instance_variable_get(:@name))
-          assert_equal("D", C.new_class({}, "D").instance_variable_get(:@name))
+        assert_equal "Hi, I am a C", @C.new.greet
+        assert_equal "Hallo, ik ben een C", @C.new.greet(:dutch)
 
-          assert C.new_class.instance_variable_defined?(:@variables)
-          assert_equal({}, C.new_class.instance_variable_get(:@variables))
-          assert_equal({}, C.new_class({}, "D").instance_variable_get(:@variables))
-        end
-
-        should "respond to var" do
-          assert !C.respond_to?(:var)
-          assert !C.new.respond_to?(:var)
-
-          assert C.new_class({:foo => "bar"}).respond_to?(:var)
-          assert C.new_class({:foo => "bar"}, "D").respond_to?(:var)
-          assert C.new_class({:foo => "bar"}).new.respond_to?(:var)
-          assert C.new_class({:foo => "bar"}, "D").new.respond_to?(:var)
-
-          assert_equal "bar", C.new_class({:foo => "bar"}).var(:foo)
-          assert_equal "bar", C.new_class({:foo => "bar"}, "D").var(:foo)
-          assert_equal "bar", C.new_class({:foo => "bar"}, "D").new.var(:foo)
-          assert_equal "bar", C.new_class({:foo => "bar"}, "D").new.var(:foo)
-        end
-
-        should "be able to create instances" do
-          assert C.new_class.new.is_a?(C)
-          assert_equal C.name, C.new_class.new.class.name
-          assert_equal C.name, C.new_class.new.class.to_s
-          assert_equal "Hi, I am a C", C.new_class.new.greet
-
-          assert C.new_class({}, "D").new.is_a?(C)
-          assert_equal "D", C.new_class({}, "D").new.class.name
-          assert_equal "D", C.new_class({}, "D").new.class.to_s
-          assert_equal "Hi, I am a D", C.new_class({}, "D").new.greet
-        end
-
-        should "call when_defined" do
-          klass = C.new_class({:translations => {:dutch => "Hallo, ik ben een {class}"}})
-          assert_equal "Hi, I am a C", klass.new.greet
-          assert_equal "Hallo, ik ben een C", klass.new.greet(:dutch)
-
-          klass = C.new_class({:translations => {:dutch => "Hallo, ik ben een {class}"}}, "D")
-          assert_equal "Hi, I am a D", klass.new.greet
-          assert_equal "Hallo, ik ben een D", klass.new.greet(:dutch)
-        end
+        assert_equal "Hi, I am a D", @D.new.greet
+        assert_equal "Hallo, ik ben een D", @D.new.greet(:dutch)
       end
     end
   end
